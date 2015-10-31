9fc46b9d7c9a263e075659a64b9bb95c9ee0b692
quarckster <quarckster@gmail.com>
2015-10-31 17:14:27 +0700

    Now change_sp.py reconnects if answer is empty

--- Python/change_sp.py
+++ Python/change_sp.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
-from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 import logging
 import sys
+import time
+from pymodbus.exceptions import ModbusException
+from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 
 logging.basicConfig()
 log = logging.getLogger()
@@ -11,12 +13,16 @@ client = ModbusClient(method="rtu",
                       baudrate=9600,
                       parity="E",
                       port="/dev/ttyUSB0",
-                      timeout=0.1,
-                      retries=3,
-                      retry_on_empty=True)
+                      timeout=0.5)
 client.connect()
-
-rr = client.read_holding_registers(0, 7, unit=1)
+registers = False
+while not registers:
+    rr = client.read_holding_registers(0, 7, unit=1)
+    print(dir(rr))
+    if hasattr(rr, "registers"):
+        registers = True
+    else:
+        time.sleep(1)
 
 print("Reg40001: {}".format(rr.getRegister(0)));
 print("Reg40002: {}".format(rr.getRegister(1)));
@@ -24,8 +30,11 @@ print("Reg40003: {}".format(rr.getRegister(2)));
 print("Reg40004: {}".format(rr.getRegister(3)));
 print("Reg40005: {}".format(rr.getRegister(4)));
 print("Reg40006: {}".format(rr.getRegister(5)));
+
 setpoint = int(-101.4856 + 1.7363171*int(sys.argv[1]))
+
 assert(setpoint > 0 and setpoint < 100)
+
 client.write_register(0, 4, unit=1)
 
 client.write_register(2, setpoint, unit=1)
