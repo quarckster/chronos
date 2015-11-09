b772d701aef1008c5298fb24841000d4086f09c1
Dmitry Misharov <quarckster@gmail.com>
2015-11-09 14:39:54 +0700

    bstat.py now is reading settings from chronos_config.json

--- Python/bstat.py
+++ Python/bstat.py
@@ -2,23 +2,20 @@
 import sys
 import serial
 import time
+from config_parser import cfg
 from pymodbus.exceptions import ModbusException
 from pymodbus.client.sync import ModbusSerialClient
 
-method = "rtu"
-baudrate = 9600
-parity = "E"
-port = "/dev/ttyUSB0"
-timeout = 0.3
+method = cfg.modbus.method
+baudrate = cfg.modbus.baudr
+parity = cfg.modbus.parity
+port = cfg.modbus.portname
+timeout = cfg.modbus.timeout
 
 # Connecting to boiler via modbus protocol
+
+
 def get_boiler_stats():
-    # boiler_stats = {"system_supply_temp": 0,
-    #                 "outlet_temp": 0,
-    #                 "inlet_temp": 0,
-    #                 "flue_temp": 0,
-    #                 "cascade_current_power": 0,
-    #                 "lead_firing_rate": 0}
     boiler_stats = [0, 0, 0, 0, 0, 0]
     try:
         modbus_client = ModbusSerialClient(method=method,
@@ -30,7 +27,7 @@ def get_boiler_stats():
     except (ModbusException, OSError) as e:
         return boiler_stats
 
-    c_to_f = lambda t: round(((9.0/5.0)*t + 32.0), 1)
+    c_to_f = lambda t: round(((9.0 / 5.0) * t + 32.0), 1)
     for i in range(3):
         try:
             # Read one register from 40006 address to get System Supply Temperature
@@ -39,17 +36,17 @@ def get_boiler_stats():
             hregs = modbus_client.read_holding_registers(6, count=1, unit=1)
             # Read 9 registers from 30003 address
             iregs = modbus_client.read_input_registers(3, count=9, unit=1)
-            boiler_stats = [c_to_f(hregs.getRegister(0)/10.0),
-                            c_to_f(iregs.getRegister(5)/10.0),
-                            c_to_f(iregs.getRegister(6)/10.0),
-                            c_to_f(iregs.getRegister(7)/10.0),
+            boiler_stats = [c_to_f(hregs.getRegister(0) / 10.0),
+                            c_to_f(iregs.getRegister(5) / 10.0),
+                            c_to_f(iregs.getRegister(6) / 10.0),
+                            c_to_f(iregs.getRegister(7) / 10.0),
                             float(iregs.getRegister(3)),
                             float(iregs.getRegister(8))]
         except (OSError, serial.SerialException, ModbusException, AttributeError, IndexError) as e:
             time.sleep(0.7)
         else:
             break
-    modbus_client.close()  
+    modbus_client.close()
     return boiler_stats
 
 if __name__ == '__main__':
