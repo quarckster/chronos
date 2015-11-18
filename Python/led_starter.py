bc0e5e8792dd4863017d16525921faa571684094
Dmitry Misharov <quarckster@gmail.com>
2015-11-18 10:01:22 +0700

    cleaning repo

--- Python/led_starter.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python
-import os
-os.system("sudo python /home/pi/Desktop/Chronos/led_trial2.py")
--- Python/pulserCode.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python2.7  
-
-import RPi.GPIO as GPIO
-
-pulserPin = 17
-p = GPIO.PWM(pulserPin, 80)
-p.start(0)
