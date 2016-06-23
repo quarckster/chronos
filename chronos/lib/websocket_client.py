6ece76620de4979767587afa2c7c8dc207a2155a
Dmitrii Misharov <dmisharo@redhat.com>
2016-06-23 11:03:15 +0200

    Removed unused js code, implemented reconnection in websocket client

--- chronos/lib/websocket_client.py
+++ chronos/lib/websocket_client.py
@@ -1,5 +1,7 @@
 import json
+import time
 import websocket
+from chronos.lib.root_logger import root_logger as logger
 
 websocket_client = None
 
@@ -9,4 +11,12 @@ def send_message(data):
     if websocket_client is None:
         websocket_client = websocket.WebSocket()
         websocket_client.connect("ws://127.0.0.1:8000/")
-    websocket_client.send(unicode(json.dumps(data)))
+    for i in range(1, 4):
+        try:
+            websocket_client.send(unicode(json.dumps(data)))
+        except Exception:
+            time.sleep(1)
+            logger.exception("Reconnecting to websocket server")
+            websocket_client = websocket.WebSocket()
+            websocket_client.connect("ws://127.0.0.1:8000/")
+            websocket_client.send(unicode(json.dumps(data)))
